3ae521052c85267ddc7b46cf6ba81bc8f8fec374
tomalovesmaya <chejabron@gmail.com>
2026-06-25 23:45:25 -0700

    make polaroid and tilt more convincing

--- script.js
+++ script.js
@@ -10,29 +10,39 @@ async function loadData() {
 
     img.onload = () => {
         const imageEl = document.getElementById("maya-image");
-        imageEl.src = image;
-    
+
+        if (img.naturalHeight > img.naturalWidth) {
+            imageEl.style.padding = "16px 56px 16px 16px";
+        } else {
+            imageEl.style.padding = "16px 16px 56px 16px";
+        }
+
         let rotation;
-    
+
         if (Math.random() < 0.06262026) {
             rotation = (Math.random() < 0.5 ? -1 : 1) * (5 + Math.random() * 3);
         } else {
             rotation = (Math.random() < 0.5 ? -1 : 1) * (0.5 + Math.random() * 2.5);
         }
-    
+
+        if (img.naturalHeight > img.naturalWidth) {
+            rotation += (Math.random() < 0.5 ? -2 : 2);
+        }
+
         imageEl.style.transform = `rotate(${rotation}deg)`;
-    
+        imageEl.src = image;
+
         const textEl = document.getElementById("text");
-    
+
         if (Math.random() < 0.002132026) {
             textEl.textContent = "yeah i prolly would";
         } else {
             const adjective =
                 data.adjectives[Math.floor(Math.random() * data.adjectives.length)];
-    
+
             textEl.innerHTML = `<strong>Maya</strong> is <em>${adjective}</em>`;
         }
-    
+
         document.getElementById("page").classList.add("show");
     };
 }
