4e8f9b69ff4bae5c2ea166f521ab0c14fb91b7f7
tbrsvn <chejabron@gmail.com>
2026-06-25 18:59:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ async function loadData() {
     const image =
         data.images[Math.floor(Math.random() * data.images.length)];
 
-    const adjective =
-        data.adjectives[Math.floor(Math.random() * data.adjectives.length)];
-
     const img = new Image();
     img.src = image;
 
     img.onload = () => {
         document.getElementById("maya-image").src = image;
-        document.getElementById("adjective").textContent = adjective;
+
+        const textEl = document.getElementById("text");
+
+        if (Math.random() < 0.01) {
+            textEl.textContent = "yeah I prolly would";
+        } else {
+            const adjective =
+                data.adjectives[Math.floor(Math.random() * data.adjectives.length)];
+
+            textEl.innerHTML = `<strong>Maya</strong> is <em>${adjective}</em>`;
+        }
 
         document.getElementById("page").classList.add("show");
     };
